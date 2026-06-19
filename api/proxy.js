e14e276f9f505fd32b27486716aefdd12fcdafec
satoknan-droid <satoknan@gmail.com>
2026-06-19 21:38:38 +0900

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,27 +1,3 @@
-export default async function handler(req, res) {
-
-  const url = req.query.url;
-
-  if (!url) {
-    return res.status(400).send("No URL");
-  }
-
-  try {
-
-    const response = await fetch(url);
-
-    let text = await response.text();
-
-    res.setHeader(
-      "Content-Type",
-      "text/html"
-    );
-
-    res.status(200).send(text);
-
-  } catch (err) {
-
-    res.status(500).send("Proxy Error");
-
-  }
-}
+text = text
+.replace(/href="\//g, `href="${url}/`)
+.replace(/src="\//g, `src="${url}/`);
